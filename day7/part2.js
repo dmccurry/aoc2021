8abca28b4716f04b1acd483d580ecb18805b8aa1
David McCurry <dmccurry@cvent.com>
2021-12-08 10:42:24 -0400

    Adding day 8

--- day7/part2.js
+++ day7/part2.js
@@ -7,13 +7,19 @@ const getT = (a, b) => {
 
 fs.readFile('./input', 'utf-8', (err, data) => {
   const starts = data.split(',');
-  const crabs = starts.map(s => parseInt(s));
-  crabs.sort((a, b) => a > b);
+  let crabs = starts.map(s => parseInt(s));
+  crabs.sort((a, b) => a - b);
+  
+  let total = crabs.reduce((a, b) => a + b, 0);
+  console.log(Math.ceil(total / crabs.length));
+  console.log(Math.floor(total / crabs.length));
   
   let minDistance = 0;
   let targets = [];
 
-  for (let i=0; i<crabs[crabs.length - 1]; i++) {
+  let finalPoint = 0;
+
+  for (let i=crabs[0]; i<crabs[crabs.length - 1]; i++) {
     if (!targets[i]) {
       let d = 0;
       for (let j=0; j<crabs.length; j++) {
@@ -22,10 +28,17 @@ fs.readFile('./input', 'utf-8', (err, data) => {
       }
       if (d < minDistance || minDistance === 0) {
         minDistance = d;
+        finalPoint = i;
       }
       targets[i] = d;
     }
   }
   console.log(`Solution is ${minDistance}`)
+  console.log(`They all moved to ${finalPoint}`);
+
+  console.log(`Your bounds were ${crabs[0]} and ${crabs[crabs.length - 1]}`)
+
+  const sum = crabs.reduce((a, b) => a + b, 0);
+  console.log(Math.floor((sum - crabs.length) / (crabs.length - 1)));
 
 });
